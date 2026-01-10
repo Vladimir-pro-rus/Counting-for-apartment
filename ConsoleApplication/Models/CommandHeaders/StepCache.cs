bf13a3115dab7c193e1c7423e73d3f816850398d
Владимир <kalibarda@mail.ru>
2026-01-10 20:26:02 +0300

    Update StepCache.cs

--- ConsoleApplication/Models/CommandHeaders/StepCache.cs
+++ ConsoleApplication/Models/CommandHeaders/StepCache.cs
@@ -7,24 +7,13 @@ namespace ConsoleExample.Models
     /// </summary>
     public class StepCache : ITelegramCache
     {
-        #region Rudements
-        public string Name { get; set; }
-        public string BirthDay { get; set; }
-        public bool ClearData()
-        {
-            this.BirthDay = string.Empty; 
-            this.Name = string.Empty;
-            return true;
-        }
-        #endregion
+        public string Balance { get; set; } // Баланс для пользователя
+        public string DataFromUser { get; set; } // Данные от пользователя
+        public string GoalFromUser { get; set; } // Цель от пользователя
+        public string CoastFromUser { get; set; } // Сумма от пользователя
+        public string Note { get; set; } // Примечание от пользователя
 
-        public string Balance { get; set; } //BalanceService для пользователя
-        public string DataFromUser { get; set; } //Данные от пользователя
-        public string GoalFromUser { get; set; } //Цель от пользователя
-        public string CoastFromUser { get; set; } //Сумма от пользователя
-        public string Note { get; set; } //Примечание от пользователя
-
-        public string WritingCell { get; set; } //Ячейка для записи
+        public string WritingCell { get; set; } // Ячейка для записи в эксель
     }
 
 
