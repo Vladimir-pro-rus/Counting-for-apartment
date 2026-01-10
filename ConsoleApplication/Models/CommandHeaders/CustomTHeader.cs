5167ece35fe7211a14e5a432f0d9885e921a41e6
Владимир <kalibarda@mail.ru>
2026-01-10 20:28:12 +0300

    Update CustomTHeader.cs

--- ConsoleApplication/Models/CommandHeaders/CustomTHeader.cs
+++ ConsoleApplication/Models/CommandHeaders/CustomTHeader.cs
@@ -5,26 +5,7 @@ namespace ConsoleExample.Models.CommandHeaders
 {
     [InlineCommand]
     public enum CustomTHeader
-    {
-        #region Rudements
-        [Description("Бесплатный ВИП")]
-        GetFreeVIP = 500,
-        [Description("Вип на 1 день")]
-        GetVipOneDay,
-        [Description("Вип на 1 неделю")]
-        GetVipOneWeek,
-        [Description("Вип на 1 месяц")]
-        GetVipOneMonth,
-        [Description("Вип навсегда")]
-        GetVipOneForever,
-        [Description("Шаг из Inline")]
-        InlineWithStep,
-        [Description("Кастомная кнопка")]
-        CustomButton,
-        [Description("Callback для календаря")]
-        CalendarCallback,
-        #endregion
-       
+    {      
         [Description("Главное меню")]
         MainMenu,
 
